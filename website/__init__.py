32060ef247081cdad579a42577726576d5733d6f
Maksymilian Zmuda <maksymilianzmuda02@gmail.com>
2024-12-19 15:52:55 +0100

    database implementation (#3)

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .views import views
 from .auth import auth
+from .models import create_database
 
 
 def create_app():
@@ -8,4 +9,11 @@ def create_app():
     app.config['SECRET_KEY'] = "sajdasljdlaskjaskldj"
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    create_database()
+
+    @app.route("/")
+    def home():
+        return "Hello, Flask with SQLite!"
+
     return app
